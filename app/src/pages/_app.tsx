fb25bfa9ca82c1ab9ef9cfbc18c2d72bcf665b80
KMKoushik <koushikmohan1996@gmail.com>
2023-04-23 00:35:12 +1000

    Add beam analytics

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -21,6 +21,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
         <title>Docs AI - Dashboard</title>
         <meta name="description" content="Create an AI support agent with your documents." />
         <link rel="icon" href="/images/favicon.ico" />
+        <script src="https://beamanalytics.b-cdn.net/beam.min.js" data-token="494f02eb-86bc-460d-ac63-db4d351eea9c" async></script>
       </Head>
       <SessionProvider session={session}>
         <Component {...pageProps} />
