98095452ef4cf150d3cbad291489f980526070fd
KMKoushik <koushikmohan1996@gmail.com>
2023-04-28 12:08:45 +1000

    Fix chat icon

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -6,11 +6,14 @@ import { api } from "~/utils/api";
 import "~/styles/globals.css";
 import Head from "next/head";
 import { env } from "~/env.mjs";
+import { useRouter } from "next/router";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
+  const router = useRouter();
+
   return (
     <>
       <Head>
@@ -24,7 +27,9 @@ const MyApp: AppType<{ session: Session | null }> = ({
         <meta property="twitter:image" content="https://docsai.app/images/app.png" />
         <link rel="icon" href="/images/favicon.ico" />
         <script src="https://beamanalytics.b-cdn.net/beam.min.js" data-token="494f02eb-86bc-460d-ac63-db4d351eea9c" async></script>
-        <script src="/embed.js" project-id="clfp5tn2a0007mc0ub8qch4x2" primary-color="#000" async></script>
+        {!router.route.startsWith("/embed") ? (
+          <script src="/embed.js" project-id="clfp5tn2a0007mc0ub8qch4x2" primary-color="#000" async></script>
+        ) : null}
       </Head>
       <SessionProvider session={session}>
         <Component {...pageProps} />
