3be88887a48959959566bef58b579ef1d6fcb448
KMKoushik <koushikmohan1996@gmail.com>
2023-05-13 15:56:30 +1000

    Fix overflow

--- app/src/utils/color.ts
+++ app/src/utils/color.ts
@@ -2,7 +2,7 @@ import tinycolor from 'tinycolor2';
 
 export const getContrastColor = (bgColor: string) => {
   if (bgColor === '#000') return '#fff'
-  if (bgColor.length != 7) return '#000'
+  if (bgColor.length != 7 && bgColor.length != 4) return '#000'
   const textColorPallete = ["#000", "#fff"]
   const contrastColor = tinycolor.mostReadable(bgColor, textColorPallete);
   return contrastColor.toHexString();
