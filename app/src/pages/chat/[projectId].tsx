195884d87c266db9483a3eb1afd52db6ebf22eb3
Shrihari Mohan <shrihari.uma.1999@gmail.com>
2023-11-10 20:37:06 +0530

    Add - reduce quality of images

--- app/src/pages/chat/[projectId].tsx
+++ app/src/pages/chat/[projectId].tsx
@@ -38,14 +38,14 @@ const QnAPage: NextPage<{ orgJson: string | null, projectJson: string | null }> 
         <title>{org.name}</title>
         <meta property="og:title" content={project.botName} />
         <meta name="description" content={project.description || 'Create AI Support Agents with your docs'} />
-        <meta property="og:image" content="https://docsai.app/images/app.png" />
+        <meta property="og:image" content="https://docsai.app/images/og-app.jpeg" />
         <meta property="og:image:height" content="630" />
         <meta property="og:image:width" content="1200" />
         <meta property="twitter:title" content={project.botName} />
         <meta property="og:url" content={`https://docsai.app/chat/${project.id}`}></meta>
         <meta property="twitter:description" content={project.description || 'Create AI Support Agents with your docs'} />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta property="twitter:image" content="https://docsai.app/images/app.png" />
+        <meta property="twitter:image" content="https://docsai.app/images/og-app.jpeg" />
         <meta property="og:type" content="website"></meta>
         <link rel="icon" href="/images/favicon.ico" />
       </Head>
