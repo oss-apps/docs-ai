a6223dbe208244bd2a029ea2d5f53c586e4b0ed6
KMKoushik <koushikmohan1996@gmail.com>
2024-05-30 15:32:24 +1000

    Migrate pinecone

--- app/src/server/docgpt/store.ts
+++ app/src/server/docgpt/store.ts
@@ -1,52 +1,69 @@
-import { PineconeClient } from "@pinecone-database/pinecone";
+import { Pinecone } from "@pinecone-database/pinecone";
 import { type DocumentType } from "@prisma/client";
 import { Document } from "langchain/document";
 import { OpenAIEmbeddings } from "langchain/embeddings";
-import { PineconeStore } from "langchain/vectorstores";
+//import { PineconeStore } from "langchain/vectorstores";
 import { env } from "~/env.mjs";
 import { RecursiveCharacterTextSplitter } from "./textSplitter";
+import { PineconeStore } from "@langchain/pinecone";
 
-
-export const getVectorIndex = async () => {
-  const client = new PineconeClient()
-  await client.init({
+export const getVectorIndex = () => {
+  const client = new Pinecone({
     apiKey: env.PINECONE_API_KEY,
-    environment: env.PINECONE_ENVIRONMENT,
   });
 
-  return client.Index(env.PINECONE_INDEX)
-}
+  return client.Index(env.PINECONE_INDEX);
+};
 
-export const loadDocumentsToDb = async (projectId: string, documentId: string, docType: DocumentType, docs: Array<Document>) => {
-  const pineconeIndex = await getVectorIndex()
-  const documents = docs.map(d => new Document({
-    pageContent: d.pageContent, metadata: { ...d.metadata, projectId, documentId, type: docType }
-  }))
+export const loadDocumentsToDb = async (
+  projectId: string,
+  documentId: string,
+  docType: DocumentType,
+  docs: Array<Document>,
+) => {
+  const pineconeIndex = getVectorIndex();
+  const documents = docs.map(
+    (d) =>
+      new Document({
+        pageContent: d.pageContent,
+        metadata: { ...d.metadata, projectId, documentId, type: docType },
+      }),
+  );
 
   const splitter = new RecursiveCharacterTextSplitter({
     chunkSize: 2000,
     chunkOverlap: 100,
-  })
+  });
 
-  const splitDocuments = await splitter.splitDocuments(documents)
-  await PineconeStore.fromDocuments(splitDocuments, new OpenAIEmbeddings(), { pineconeIndex, namespace: projectId })
-}
-
+  const splitDocuments = await splitter.splitDocuments(documents);
+  await PineconeStore.fromDocuments(splitDocuments, new OpenAIEmbeddings(), {
+    pineconeIndex,
+    namespace: projectId,
+  });
+};
 
 export const getVectorDB = async (projectId: string) => {
-  const pineconeIndex = await getVectorIndex()
+  const pineconeIndex = getVectorIndex();
 
   const vectorStore = await PineconeStore.fromExistingIndex(
     new OpenAIEmbeddings(),
-    { pineconeIndex, namespace: projectId }
+    { pineconeIndex, namespace: projectId },
   );
 
-  return vectorStore
-}
+  return vectorStore;
+};
 
-export const deleteDocumentVector = async (projectId: string, documentId: string) => {
-  const index = await getVectorIndex()
+export const deleteDocumentVector = async (
+  projectId: string,
+  documentId: string,
+) => {
+  const index = getVectorIndex().namespace(projectId);
   if (index) {
-    await index._delete({ deleteRequest: { namespace: projectId, filter: { documentId } } })
+    // const page = index.listPaginated()
+    // await index.de({
+    //   deleteRequest: { namespace: projectId, filter: { documentId } },
+    // });
+
+    return Promise.resolve(true);
   }
-}
+};
